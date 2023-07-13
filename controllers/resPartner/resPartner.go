ff2766645c35e2b6c8995d38fff6c34c34a7877b
Muhammad Naufal <mnaufal0999@gmail.com>
2023-07-13 15:54:27 +0700

    add comment to controller

--- controllers/resPartner/resPartner.go
+++ controllers/resPartner/resPartner.go
@@ -2,17 +2,34 @@ package controller
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 
 	send "github.com/emnopal/go_postgres/models/jsonResponse"
 	resPartner "github.com/emnopal/go_postgres/models/resPartner"
+	resPartnerSchema "github.com/emnopal/go_postgres/schemas/db/resPartner"
 	"github.com/gin-gonic/gin"
 )
 
 type ResPartnerController struct{}
 
+// Get all res partner column
+//
+// URL: /?page={}&limit={}&search={}&ignore_performance={}
+//
+// Params:
+//
+// - page (uint): cursor for pagination in database
+//
+// - limit (uint): limit for database query (to enhance performance)
+//
+// - search (string): search for some string inside query
+//
+// - ignore_performance (boolean): ignoring for some performance tweak.
+// If true, some performance tweak will be ignored.
+// Note: i don't recommend to use this parameter
 func (attr *ResPartnerController) GetResPartner(c *gin.Context) {
 	queryParams := c.Request.URL.Query()
 	pageParams := queryParams.Get("page")
@@ -76,6 +93,13 @@ func (attr *ResPartnerController) GetResPartner(c *gin.Context) {
 	j.SendJsonGet(result, err)
 }
 
+// Get res partner based on id
+//
+// URL: /:id
+//
+// Params:
+//
+// - id (uint): unique id for res.partner table
 func (attr *ResPartnerController) GetResPartnerById(c *gin.Context) {
 	RP := &resPartner.ResPartner{}
 	id := c.Param("id")
@@ -86,4 +110,34 @@ func (attr *ResPartnerController) GetResPartnerById(c *gin.Context) {
 		j.CustomErrorRespStatus = http.StatusNotFound
 	}
 	j.SendJsonGet(result, err)
+}
+
+// Create res partner
+//
+// URL: /
+func (attr *ResPartnerController) CreateResPartner(c *gin.Context) {
+	j := &send.JsonSendGetHandler{GinContext: c}
+	RP := &resPartner.ResPartner{}
+	var request resPartnerSchema.CreateResPartner
+
+	if err := c.ShouldBindJSON(&request); err != nil {
+		if err.Error() == "EOF" {
+			j.CustomErrorLogMsg = "JSON is null"
+			j.CustomErrorRespMsg = "JSON is null"
+			j.CustomErrorRespStatus = http.StatusBadRequest
+		}
+		j.SendJsonGet(nil, err)
+		return
+	}
+
+	if err := RP.CreateResPartner(&request); err != nil {
+		j.SendJsonGet(nil, err)
+		return
+	}
+
+	j.CustomSuccessLogMsg = fmt.Sprintf("Success send data to: %s", j.GinContext.Request.URL.RequestURI())
+	j.CustomSuccessRespMsg = "Success"
+	j.CustomSuccessRespStatus = http.StatusCreated
+
+	j.SendJsonGet(nil, nil)
 }
